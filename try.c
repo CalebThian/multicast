e63b052e32412f1979cb49779e856e9603f1ab84
Caleb Thian Jia Le <calebthian@gmail.com>
2021-04-17 20:12:03 +0800

    Add the file transferred and file generate code,i.e. try.c

---
+++ try.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	FILE *fp = fopen("alphabet.txt","w");
+	int len = 0;
+	int mb = 0;
+	char alpha[26];
+	for(int i = 0;i<26;++i){
+		alpha[i]='a'+i;
+	}
+	while(mb<10*1024){
+		len = 0;
+		while(len<1024){
+			fprintf(fp,alpha);
+			len+=26;
+		}
+		mb+=1;
+	}
+	fclose(fp);
+	return 0;
+}
